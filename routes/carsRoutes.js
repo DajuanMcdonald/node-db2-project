0909051c1868fc3bbbf1e7671d63eefba775ddbf
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-04-24 16:43:48 -0400

    imported db into routes file

--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const db = require('../data/car-dealer.db3')
 
 const server = express();
 
 server.get('/', (req, res) => {
+    
     res.status(200).send('<h1>Database Schema Design</h1>')
 })
 
