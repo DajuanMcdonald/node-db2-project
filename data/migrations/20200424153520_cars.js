eba604abc3647640c148406d9388ec0c5fc65250
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-04-24 16:07:20 -0400

    added migration configuration and created table

---
+++ data/migrations/20200424153520_cars.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', function (table) {
+        table.increments();
+        table.string('make', 255).notNullable();
+        table.string('model', 255).notNullable();
+        table.string('vin', 255).notNullable();
+        table.integer('milage').notNullable();
+        table.string('transmission');
+        table.string('title');
+    })
+  
+};
+
+exports.down = function(knex) {
+  
+};
--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/car-dealer.db3'
     },
 
-    useNullAsDefault: true
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: './data/migrations'
+    }
   }
 
 };
