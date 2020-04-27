1aa1546d1e861b0dd50a0c1ecf733a5fb42444af
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-04-26 23:50:17 -0400

    added DELETE request

--- cars/carsRoutes.js
+++ cars/carsRoutes.js
@@ -58,4 +58,19 @@ router.put('/:id', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal Server Error: 500'}))
 })
 
+//remove car from database : DELETE/REMOVE
+router.delete('/:id', (req, res) => {
+    db('cars').where({id : req.params.id}).del()
+    .then( car => {
+        if (car > 0) {
+            res.status(200).json({message: 'Record deleted Successfully'})
+
+        } else {
+            res.status(404).json({message: 'Car not found'})
+        }
+    }).catch(err => {
+        res.status(500).json({message: 'Internal Server Error: 500'})
+    })
+})
+
 module.exports = router;
