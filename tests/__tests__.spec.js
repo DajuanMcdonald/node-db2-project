24c75204df88eb72b8143f6cea89630d78662bcb
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-04-26 23:12:12 -0400

    reverted one test

--- tests/__tests__.spec.js
+++ tests/__tests__.spec.js
@@ -1,6 +1,5 @@
 const knex = require('../data/config');
 const request = require('supertest');
-const server = require('../cars/carsRoutes');
 
 describe('CRUD operations for cars table ', () => {
     beforeAll((done) => {
@@ -14,17 +13,33 @@ describe('CRUD operations for cars table ', () => {
         console.log('Testing library up and running')
     });
 
-})
-
-describe('Getting back status codes', () => {
-        test('should return OK status code from index route', () => {
-            const expectedStatusCode = 200;
-        
-            let response = request(knex).get('/')
-            .then( res => {
-                response = res;
-                expect(response.status).toEqual(expectedStatusCode);
-            })
-        
+    test('should return OK status code from index route', () => {
+        const expectedStatusCode = 200;
+    
+        let response = request(knex).get('/')
+        .then( res => {
+            response = res;
+            expect(response.status).toEqual(expectedStatusCode);
+        }).catch(err => {
+            console.log(err)
         })
+    
+    })
+
+
+
+    // test('Created a post request', (done) => {
+    //     request(knex)
+    //     .post('/cars')
+    //     .send({make: 'Chevy', model: 'Impala', vin: '6543210', milage: '4300'})
+    //     .set('Accept', 'application/json')
+    //     .expect(201)
+    //     .then((response) => {
+    //         expect(response.body).toBe('object')
+    //         done();
+            
+    //     })
+    // })
+
+
 })
