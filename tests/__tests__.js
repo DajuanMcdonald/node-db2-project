a23c99fc351d3ca1bfe52ca428abbc0edd7f7e50
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-04-26 20:22:41 -0400

    working on testing

--- tests/__tests__.js
+++ tests/__tests__.js
@@ -1,10 +1,14 @@
 const knex = require('../data/config');
 
 describe('CRUD operations for cars table', () => {
-    BeforeUnloadEvent(() => {
+    before((done) => {
         knex.migrate.latest()
         .then(() => {
             return knex.seed.run();
-        })
+        }).then(() => done())
     })
+
+    test('Testing library able to read and run test', function () {
+        console.log('Testing library up and running')
+    });
 })
